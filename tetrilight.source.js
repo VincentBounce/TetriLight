4943c5f17ba4cdf13cb7c5c660599fb4dcab9718
VincentBounce <vintive@gmail.com>
2024-03-06 19:17:33 +0400

    Misc

--- tetrilight.source.js
+++ tetrilight.source.js
@@ -110,9 +110,9 @@ const DURATIONS                 = { // tetris durations, periods in ms
     lostMessageDuration         : 3500, // 3500 ms, period to display score
     softDropPeriod              : 50, // 0050 ms, if this is max DropDuration
     initialDropPeriod           : 1100 }; // 0700 ms, >= _softDropPeriod, decrease during game, increase for #DEBUG, incompressible duration by any key excepted pause
-//const FONTS                   = { scoreFont: 'Ubuntu', messageFont: 'Permanent Marker' }; // online fonts
-const FONTS                   = { scoreFont: 'Arial, Helvetica, sans-serif', messageFont: 'Impact, Charcoal, sans-serif' }; // web safe fonts = offline fonts
-//const FONTS                   = { scoreFont: 'Arial, sans-serif', messageFont: 'Gill Sans, sans-serif' }; // web safe fonts = offline fonts
+const FONTS                   = { scoreFont: 'Ubuntu', messageFont: 'Permanent Marker' }; // online fonts
+//const FONTS                   = { scoreFont: 'Arial, Helvetica, sans-serif', messageFont: 'Impact, Charcoal, sans-serif' }; // web safe fonts = offline fonts
+//const FONTS                   = { scoreFont: 'Tahoma, sans-serif', messageFont: 'Gill Sans, sans-serif' }; // web safe fonts = offline fonts
 const SOUNDS                  = { 
     landFX                    : {ext: 'wav'},
     rotateFX                  : {ext: 'wav'},
